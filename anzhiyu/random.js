5e59defe961db7a01a0bdcdcb8067afb9e8065cd
Weisifengbuxi <sun060729@qq.com>
2025-07-25 20:38:01 +0800

    Site updated: 2025-07-25 20:38:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/18/hello-world/","2025/07/19/My-New-Post/"];function toRandomPost(){
+var posts=["2025/07/19/My-New-Post/","2025/07/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
