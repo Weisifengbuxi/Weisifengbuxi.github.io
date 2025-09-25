32e3e2b312e63fd5d11c1ae94e3d4d02ea737f34
Weisifengbuxi <sun060729@qq.com>
2025-09-26 01:03:02 +0800

    Site updated: 2025-09-26 01:02:59

--- js/main.js
+++ js/main.js
@@ -1838,6 +1838,11 @@ function initWeatherWidget() {
     anzhiyu.addEventListenerConsoleMusicList(false);
     anzhiyu.initPaginationObserver();
 
+
+    if (window.AuroraPerformanceOptimizer && typeof window.AuroraPerformanceOptimizer.initBackgroundToggle === 'function') {
+     window.AuroraPerformanceOptimizer.initBackgroundToggle();
+    }
+
     setTimeout(() => {
       setInputFocusListener();
       if (typeof addFriendLinksInFooter === "function") {
--- js/utils.js
+++ js/utils.js
@@ -1341,7 +1341,16 @@ const anzhiyu = {
     }
     authorInfoSayHiElement.textContent = randomGreeting;
   },
+
+  toggleBackgroundMode: function() {
+    if (window.AuroraPerformanceOptimizer && typeof window.AuroraPerformanceOptimizer.toggleBackground === 'function') {
+      window.AuroraPerformanceOptimizer.toggleBackground();
+    } else {
+      console.warn('背景切换功能未加载');
+    }
+  },
 };
+
 
 const anzhiyuPopupManager = {
   queue: [],
